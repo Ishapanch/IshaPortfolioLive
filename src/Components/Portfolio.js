3e4f82af60fb12e284858cad0ae7d9265fcf335b
Ishapanch <inp2202@gmail.com>
2024-02-27 22:20:36 +0530

    portfolio gallery start , style pending

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import './Portfolio.css'
+import React from 'react';
+import './Portfolio.css';
+import { ImageGallery } from "react-image-grid-gallery";
+import logo from "../assets/logo/isha-logo.png";
+import AboutImg from '../assets/isha-about.png'
+import Img1 from '../assets/banner/isha-banner.png'
+
+const imagesArray = [
+  {
+    alt: "Image1's alt text",
+    src: logo,
+  },
+  {
+    alt: "Image1's alt text",
+    src: AboutImg,
+  },
+  {
+    alt: "Image1's alt text",
+    src: Img1,
+  },
+
+];
 
 export default function Portfolio() {
   return (
-    <div className='container'>
-        <div class="green-title">My Work</div>
-        <h3 class="heading-one">Check My Portfolio</h3>
+    <div className="container">
+      <div class="green-title">Services i offer to my clients</div>
+            <h3 class="heading-one">My Services</h3>
+      <ImageGallery
+        imagesInfoArray={imagesArray}
+        columnWidth="100%"
+        gapSize={24}
+      />
     </div>
-  )
+  );
 }
