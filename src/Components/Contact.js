f26a0c16b1864e19fdb28c1fd5370c911c883315
Ishapanch <inp2202@gmail.com>
2024-03-07 21:54:47 +0530

    contact page designing in process part 2

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import './AboutPage.css'
-import './Contact.css'
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import "./AboutPage.css";
+import "./Contact.css";
+import Form from "react-bootstrap/Form";
 
 export default function Contact() {
   return (
@@ -15,16 +15,19 @@ export default function Contact() {
             </div>
           </div>
           <div className="col-lg-7 col-12">
-            
-              <Form>
+            <Form>
               <div className="row">
-                <div className="col-lg-6 col-12">
+                <div className="col-lg-12 col-12">
                   <Form.Group
-                    className="mb-3"
+                    className="mb-3 "
                     controlId="exampleForm.ControlInput1"
                   >
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Name" />
+                    <Form.Control
+                      type="text"
+                      className="form-grp"
+                      placeholder="Enter Name"
+                    />
                   </Form.Group>
                 </div>
                 <div className="col-lg-6 col-12">
@@ -33,46 +36,58 @@ export default function Contact() {
                     controlId="exampleForm.ControlInput1"
                   >
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Email" />
+                    <Form.Control
+                      type="email"
+                      className="form-grp"
+                      placeholder="Enter Email"
+                    />
                   </Form.Group>
                 </div>
                 <div className="col-lg-6 col-12">
-                  <Form.Select aria-label="Services">
+                  <Form.Label>My Services</Form.Label>
+                  <Form.Select aria-label="Services" className="form-grp">
                     <option>Open this select services</option>
                     <option value="web">Web Designing</option>
                     <option value="graphic">Graphhic Designing</option>
                     <option value="both">Both</option>
                   </Form.Select>
                 </div>
-                <div className="col-lg-6 col-12">
-                  {["radio"].map((type) => (
-                    <div key={`default-${type}`} className="mb-3">
-                      <Form.Check // prettier-ignore
-                        type={type}
-                        id={`default-${type}`}
-                        label={`default ${type}`}
+                <div className="col-lg-12 col-12">
+                  <Form.Label>Select Any </Form.Label>
+                  <div className="radio-btn">
+                  {["Full Time", "Freelancing", "Part Time"].map((gender) => (
+                    <div
+                      key={`default-${gender}`}
+                      className="mb-3 form-radio-btn"
+                    >
+                      <Form.Check
+                        type="radio"
+                        id={`default-${gender}`}
+                        name="gender"
+                        value={gender}
+                        label={gender.charAt(0).toUpperCase() + gender.slice(1)} // Capitalize first letter
                       />
                     </div>
                   ))}
+                  </div>
                 </div>
-
-                
-            </div>
-              </Form>
+                <div className="col-lg-12 col-12">
+                <div className="btn- mt-3">
+                  <a to="/" className="btn-green">
+                    Connect
+                  </a>
+                </div>
+                </div>
+              </div>
+            </Form>
           </div>
           <div className="col-lg-5 col-12">
             <div className="padding-class">
-              <div className="green-title">Who am i?</div>
-              <h3 className="heading">
-                I'm Isha Pancholi, a visual UX/UI Designer and Frontend Web
-                Developer
-              </h3>
+              <div className="green-title">Contact Me ?</div>
+              <h3 className="heading">I'm Isha Pancholi</h3>
               <p className="description">
-                I am a Designer and have been building noteworthy UX/UI designs
-                and websites for years, which comply with the latest design
-                trends. I help convert a vision and an idea into meaningful and
-                useful products. Having a sharp eye for product evolution helps
-                me prioritize tasks, iterate fast and deliver faster.
+                Always available for freelance work if the right project comes
+                along, Feel free to contact me!
               </p>
               <hr className="seperator-hr" />
               <ul className="no-list details-my">
@@ -90,37 +105,7 @@ export default function Contact() {
                   <span>From:</span> Vadodara, Gujarat, India
                 </li>
               </ul>
-              <div className="bottom-prt">
-                <div className="btn-sec">
-                  <a to="/" className="btn-green">
-                    Download CV
-                  </a>
-                </div>
-                <div className="social-media">
-                  <ul className="no-list details-my">
-                    <li>
-                      <a to="/">
-                        <i class="fab fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a to="/">
-                        <i class="fab fa-linkedin"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a to="/">
-                        <i class="fab fa-github"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a to="/">
-                        <i class="fab fa-instagram"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+             
             </div>
           </div>
         </div>
